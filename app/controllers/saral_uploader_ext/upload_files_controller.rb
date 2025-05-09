88934dad8dcddeb9df51d2d09313b32898e3b804
Chitranshoo Prakash <chitranshoo.prakash@jarvis.consulting>
2025-05-09 12:23:59 +0530

    get signed url using file_path

--- app/controllers/saral_uploader_ext/upload_files_controller.rb
+++ app/controllers/saral_uploader_ext/upload_files_controller.rb
@@ -55,6 +55,40 @@ module SaralUploaderExt
       render json: { success: false, message: e.message }, status: :bad_request
     end
 
+    def get_signed_url_using_file_path
+      bucket_name = @app_config[:gcloud_bucket]
+      unless bucket_name.present?
+        raise SaralUploaderExt::CustomError.new('Bucket name must be present', "'G_CLOUD_BUCKET' is not found in .env file in main rails application")
+      end
+
+      gcloud_project_id = @app_config[:gcloud_project_id]
+      unless gcloud_project_id.present?
+        raise SaralUploaderExt::CustomError.new('Gcloud project ID must be present', "'G_CLOUD_PROJECT_ID' is not found in .env file in main rails application")
+      end
+
+      gcloud_keyfile = @app_config[:gcloud_keyfile]
+      unless gcloud_keyfile.present?
+        raise SaralUploaderExt::CustomError.new('Gcloud keyfile must be present', "'G_CLOUD_KEYFILE' is not found in .env file in main rails application")
+      end
+
+      file_path = params[:file_path]
+      unless file_path.present?
+        raise SaralUploaderExt::CustomError.new('File path must be present', "File path must be present in 'file_path' key")
+      end
+
+      storage = Google::Cloud::Storage.new(project_id: gcloud_project_id, credentials: gcloud_keyfile)
+      bucket = storage.bucket(bucket_name)
+
+      raise 'Bucket not found' if bucket.nil?
+
+      expiration_time = @app_config[:signed_url_expiration_time_in_seconds].presence&.to_i || (15 * 60) # default expiration time is 15 minutes
+
+      url = bucket.signed_url(file_path.to_s, expires: expiration_time, version: :v4)
+      raise 'File not found in this file_path' if url.nil?
+
+      url
+    end
+
     def delete_file_from_bucket
       bucket_name = @app_config[:gcloud_bucket]
       unless bucket_name.present?
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SaralUploaderExt::Engine.routes.draw do
   get 'upload_files/generate_upload_signed_url', to: 'upload_files#generate_upload_signed_url'
   get 'upload_files/delete_file_from_bucket', to: 'upload_files#delete_file_from_bucket'
+  get 'upload_files/get_signed_url_using_file_path', to: 'upload_files#get_signed_url_using_file_path'
 end
