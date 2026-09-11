37ecadbe32678e54d18a8569b2e1328aeaf8f488
dhawal <dhawal.rastogi@jarvis.consulting>
2026-09-11 10:43:26 +0530

    soft delete google version fix

--- lib/saral_uploader_ext/engine.rb
+++ lib/saral_uploader_ext/engine.rb
@@ -1,5 +1,15 @@
+require_relative 'patches/google_storage_soft_deleted_patch'
+
 module SaralUploaderExt
   class Engine < ::Rails::Engine
     isolate_namespace SaralUploaderExt
+
+    config.after_initialize do
+      if defined?(Google::Apis::StorageV1::StorageService)
+        Google::Apis::StorageV1::StorageService.prepend(
+          SaralUploaderExt::Patches::GoogleStorageSoftDeletedPatch
+        )
+      end
+    end
   end
 end
